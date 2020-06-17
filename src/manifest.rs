ab16ed77e6b4fde4f7337d45e3bd7a599f213032
sparky8251 <sparky@possumlodge.me>
2020-06-17 16:03:49 -0400

    Now with GUID validation checks

--- src/manifest.rs
+++ src/manifest.rs
@@ -3,7 +3,6 @@ use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
-use uuid::Uuid;
 
 pub fn load_manifest(file: &PathBuf) -> Result<Vec<Plugin>> {
     let mut file = File::open(&file)?;
@@ -16,16 +15,16 @@ pub fn load_manifest(file: &PathBuf) -> Result<Vec<Plugin>> {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Plugin {
     category: String,
-    guid: Uuid,
-    name: String,
+    pub guid: String,
+    pub name: String,
     overview: String,
     owner: String,
     description: String,
-    versions: Vec<PluginVersion>,
+    pub versions: Vec<PluginVersion>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct PluginVersion {
+pub struct PluginVersion {
     checksum: String, // TODO: See if I can limit this to valid md5 hashes
     changelog: String,
     name: String,
@@ -33,7 +32,7 @@ struct PluginVersion {
     target_abi: String, // TODO: see if I can limit this to semver representation
     #[serde(rename = "sourceUrl")]
     source_url: String, // TODO: Limit this to valid URLs only.
-    filename: String,  // TODO: See if I can limit this to a "valid" filename
-    timestamp: String, // TODO: See if I can limit this to valid timestamps only
-    version: String, // TODO: See if I can limit this to 4 part SemVer with only the first part allowed to be non-zero
+    filename: String,    // TODO: See if I can limit this to a "valid" filename
+    timestamp: String,   // TODO: See if I can limit this to valid timestamps only
+    pub version: String, // TODO: See if I can limit this to 4 part SemVer with only the first part allowed to be non-zero
 }
--- src/validate.rs
+++ src/validate.rs
@@ -1,8 +1,67 @@
-use crate::manifest::load_manifest;
+use crate::manifest::{load_manifest, Plugin};
+use std::fmt;
 use std::path::PathBuf;
+use uuid::Uuid;
+
+#[derive(Debug, Default)]
+struct ValidationReport {
+    guid: Option<Vec<GuidError>>,
+}
+
+#[derive(Debug)]
+struct GuidError {
+    plugin_name: String,
+    error: uuid::Error,
+}
+
+impl ValidationReport {
+    fn is_none(&self) -> bool {
+        self.guid.is_none()
+    }
+}
+
+impl fmt::Display for ValidationReport {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.is_none() {
+            write!(f, "Manifest has passed all validation checks!")
+        } else {
+            let mut results = String::new();
+            if let Some(v) = &self.guid {
+                for e in v {
+                    results.push_str(&format!(
+                        "Plugin {} failed validation! GUID {}.\n",
+                        e.plugin_name, e.error
+                    ));
+                }
+            }
+            write!(f, "{}", results.trim())
+        }
+    }
+}
 
 pub fn validate_manifest(file: PathBuf) {
     let manifest = load_manifest(&file).expect("failed to load manifest");
+    let mut report = ValidationReport::default();
+    report.guid = validate_guid(&manifest);
+    println!("{}", report)
+}
 
-    println!("Manifest passed all validation checks!")
+fn validate_guid(manifest: &[Plugin]) -> Option<Vec<GuidError>> {
+    let mut results = Vec::new();
+
+    for p in manifest {
+        match Uuid::parse_str(&p.guid) {
+            Ok(_) => (),
+            Err(e) => results.push(GuidError {
+                plugin_name: p.name.clone(),
+                error: e,
+            }),
+        }
+    }
+
+    if results.is_empty() {
+        None
+    } else {
+        Some(results)
+    }
 }
